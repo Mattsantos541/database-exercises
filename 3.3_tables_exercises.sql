2057bcf84ff39effd7e0b2a2543ba55deebd42ad
Matt Santos <mattsantos@Matts-MacBook-Air-2.local>
2019-09-03 15:39:15 -0500

    commit #3

--- 3.3_tables_exercises.sql
+++ 3.3_tables_exercises.sql
@@ -7,3 +7,8 @@ describe employees;
 -- Which table(s) do you think contain a numeric type column?
 -- Which table(s) do you think contain a string type column?
 -- Which table(s) do you think contain a date type column?
+
+describe employees;
+describe departments;
+
+select *from titles;
