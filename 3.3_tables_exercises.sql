f1777f91423ac053af117b0969f8c080d8f266ed
Matt Santos <mattsantos@Matts-MacBook-Air-2.local>
2019-09-03 15:28:51 -0500

    SQL employees

---
+++ 3.3_tables_exercises.sql
@@ -0,0 +1,8 @@
+USE employees;
+
+SHOW tables;
+
+describe employees;
+-- Which table(s) do you think contain a numeric type column?
+-- Which table(s) do you think contain a string type column?
+-- Which table(s) do you think contain a date type column?
