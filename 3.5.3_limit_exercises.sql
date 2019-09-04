ef6503a1b02b761d84f489434a06c99c016151df
Matt Santos <mattsantos@Matts-MacBook-Air-2.local>
2019-09-04 14:51:23 -0500

    3rd commit

--- 3.5.3_limit_exercises.sql
+++ 3.5.3_limit_exercises.sql
@@ -1,11 +1,17 @@
-##Limit Exercises
-select distinct title from titles;
+-- Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column named full_name.
+select * from employees where last_name like "E%E" order by emp_no asc;
+select CONCAT(first_name," ", last_name) from employees where last_name like "E%E";
 
--- List the first 10 distinct last name sorted in descending order. Your result should look like this:
-select distinct last_name from employees
-order by last_name desc limit 10;
+-- Convert the names produced in your last query to all uppercase;
+select upper(CONCAT(first_name," ", last_name)) from employees where last_name like "E%E";
 
--- Find your query for employees born on Christmas and hired in the 90s from order_by_exercises.sql. Update it to find just the first 5 employees. Their names should be:
-select distinct first_name, last_name from employees where hire_date between "1990-01-01" and "1999-12-31" and birth_date like "%12-25" order by birth_date asc, hire_date desc 5;
+-- For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE())
+select datediff(curdate(), hire_date) from employees where birth_date like "%12-25" and hire_date > "1990"and hire_date < "1999";
 
--- Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the query to find the tenth page of results. The employee names should be:
+
+-- Find the smallest and largest salary from the salaries table.
+select * from salaries;
+select min(salary) from salaries;
+select max(salary) from salaries;
+
+-- Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
