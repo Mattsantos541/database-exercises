7241d05d6929b9e0054487cb1a7b0164fb498862
Matt Santos <mattsantos@Matts-MacBook-Air-2.local>
2019-09-04 11:03:10 -0500

    Finished order by

--- 3.5.2_order_by_exercises.sql
+++ 3.5.2_order_by_exercises.sql
@@ -1,1 +1,30 @@
 ### Order by exercises
+
+-- Create a new file named 3.5.2_order_by_exercises.sql and copy in the contents of your exercise from the previous lesson.
+
+
+-- Modify your first query to order by first name. The first result should be Irena Reutenauer and the last result should be Vidya Simmen.
+select * from employees where first_name ='Irena' or first_name= 'Vidya' or first_name='Maya' order by first_name;
+
+
+-- Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
+select * from employees where first_name in ('Irena', 'Vidya', 'Maya') order by first_name desc, last_name asc;
+
+-- Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+select * from employees where first_name in ('Irena', 'Vidya', 'Maya') order by last_name asc, first_name desc;
+
+
+-- Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
+select * from employees where last_name like "E%" order by emp_no;
+
+
+-- Now reverse the sort order for both queries.
+select * from employees where first_name in ('Irena', 'Vidya', 'Maya') order by last_name desc, first_name asc;
+
+select * from employees where last_name like "E%" order by emp_no asc;
+
+
+
+-- Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+select * from employees where hire_date between "1990-01-01" and "1999-12-31" and birth_date like "%12-25" order by hire_date desc;
+
