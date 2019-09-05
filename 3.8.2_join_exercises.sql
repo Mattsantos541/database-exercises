84174a233e33794bd52542dffa75e2038c768259
Matt Santos <mattsantos541@gmail.com>
2019-09-05 15:44:55 -0500

    4th commit

--- 3.8.2_join_exercises.sql
+++ 3.8.2_join_exercises.sql
@@ -26,3 +26,15 @@ on titles.emp_no = dept_emp.emp_no
 join departments
 on dept_emp.dept_no = departments.dept_no
 where departments.dept_name = "customer service"
+
+-- Find the current salary of all current managers.
+select distinct departments.dept_name,
+CONCAT(employees.first_name, " ", employees.last_name) as department_manager,
+salaries.salary
+from departments 
+join dept_manager
+on departments.dept_no = dept_manager.dept_no
+join employees 
+on employees.emp_no = dept_manager.emp_no
+join salaries
+on employees.emp_no = salaries.emp_no;
