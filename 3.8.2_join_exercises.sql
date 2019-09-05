174741c51e6ea003d8b3b0035f2ba4cd04a8d3bc
Matt Santos <mattsantos541@gmail.com>
2019-09-05 15:18:47 -0500

    second commit gender

--- 3.8.2_join_exercises.sql
+++ 3.8.2_join_exercises.sql
@@ -1,8 +1,19 @@
 ##join exercises
 -- Using the example in the Associative Table Joins section as a guide, write a query that shows each department along with the name of the current manager for that department.
-select depatment.dept_name,
-CONCAT(first_name, " ", last_name) as department_name, department_manager,
+select departments.dept_name,
+CONCAT(employees.first_name, " ", employees.last_name) as department_manager
 from departments 
 join dept_manager
 on departments.dept_no = dept_manager.dept_no
-join employees on employees.emp_no = dept_manager.emp_no;
+join employees 
+on employees.emp_no = dept_manager.emp_no;
+
+-- Find the name of all departments currently managed by women.
+select departments.dept_name,
+CONCAT(employees.first_name, " ", employees.last_name) as department_manager
+from departments 
+join dept_manager
+on departments.dept_no = dept_manager.dept_no
+join employees 
+on employees.emp_no = dept_manager.emp_no
+where employees.gender = "F";
