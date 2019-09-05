d138552e90a7c29c93d4755fc023a0f77f43e6c4
Matt Santos <mattsantos541@gmail.com>
2019-09-05 14:25:13 -0500

    commit #1

--- 3.8.2_join_exercises.sql
+++ 3.8.2_join_exercises.sql
@@ -1,1 +1,8 @@
 ##join exercises
+-- Using the example in the Associative Table Joins section as a guide, write a query that shows each department along with the name of the current manager for that department.
+select depatment.dept_name,
+CONCAT(first_name, " ", last_name) as department_name, department_manager,
+from departments 
+join dept_manager
+on departments.dept_no = dept_manager.dept_no
+join employees on employees.emp_no = dept_manager.emp_no;
