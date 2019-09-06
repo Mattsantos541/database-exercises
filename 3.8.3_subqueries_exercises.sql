c8498bcdf9699d03bd67dd12125f7f8c3919b42b
Matt Santos <mattsantos541@gmail.com>
2019-09-06 09:34:02 -0500

    #2

--- 3.8.3_subqueries_exercises.sql
+++ 3.8.3_subqueries_exercises.sql
@@ -1,1 +1,10 @@
 ####Subqueries
+-- #1 Find all the employees with the same hire date as employee 101010 using a sub-query.69 Rows
+select * from employees
+where hire_date = "1986-06-26" limit 69;
+
+-- #2 Find all the titles held by all employees with the first name Aamod. 314 total titles, 6 unique titles
+select distinct title from titles
+where emp_no
+in (
+select emp_no from employees where first_name = "Aamod");
