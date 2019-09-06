76bae052ddc137d5f1615f13cc7eaf65a0403b6a
Matt Santos <mattsantos541@gmail.com>
2019-09-06 14:49:35 -0500

    1st

--- 3.9_temporary_tables.sql
+++ 3.9_temporary_tables.sql
@@ -1,1 +1,7 @@
 ### Temp Tables
+Using the example from the lesson, re-create the employees_with_departments table.
+
+Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
+Update the table so that full name column contains the correct data
+Remove the first_name and last_name columns from the table.
+What is another way you could have ended up with this same table?
