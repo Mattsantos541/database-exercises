ad14a5d810fcc8ee607b7efb5f49a30b51fc46a2
Matt Santos <mattsantos541@gmail.com>
2019-09-06 15:21:40 -0500

    second

--- 3.9_temporary_tables.sql
+++ 3.9_temporary_tables.sql
@@ -5,3 +5,15 @@ Add a column named full_name to this table. It should be a VARCHAR whose length 
 Update the table so that full name column contains the correct data
 Remove the first_name and last_name columns from the table.
 What is another way you could have ended up with this same table?
+
+
+CREATE TEMPORARY TABLE employees_with_departments 
+SELECT emp_no, first_name, last_name, dept_no, dept_name
+FROM employees
+JOIN dept_emp USING(emp_no)
+JOIN departments USING(dept_no)
+LIMIT 100;
+
+alter table full_name varchar(256)
+
+alter table drop first_name, last_name;
