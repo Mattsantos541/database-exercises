cbfa3402b99fad4e12615f7347993928809202f1
Matt Santos <mattsantos541@gmail.com>
2019-09-06 10:26:50 -0500

    #4

--- 3.8.3_subqueries_exercises.sql
+++ 3.8.3_subqueries_exercises.sql
@@ -9,13 +9,20 @@ where emp_no
 in (
 select emp_no from employees where first_name = "Aamod");
 
--- How many people in the employees table are no longer working for the company?
+-- #3 How many people in the employees table are no longer working for the company?
 select count(*)
 from employees
 where emp_no in (select emp_no from dept_emp where to_date > "9999-01-01");
 
--- Find all the current department managers that are female.
+-- #4 Find all the current department managers that are female.
 select first_name, last_name
 from employees
 join dept_manager on employees.emp_no = dept_manager.emp_no
 where employees.gender = "F";
+
+/* #4Find all the employees that currently have a higher than average salary.
+154543 rows in total. Here is what the first 5 rows will look like: */
+
+select first_name, last_name
+from employees 
+where emp_no in (salaries where salary > (select avg(salary) from salaries);
