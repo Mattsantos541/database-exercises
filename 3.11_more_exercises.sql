7ad16b76ed40f4102907cb4276e4f0de623be913
Matt Santos <mattsantos541@gmail.com>
2019-09-09 10:41:08 -0500

    sakila #2

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -48,3 +48,11 @@ order by avg(LifeExpectancy);
 use sakila;
 select lower(first_name), lower(last_name)
 from actor;
+
+/*You need to find the ID number, first name, and last name of an actor, of 
+whom you know only the first name, "Joe." What is one query would you could 
+use to obtain this information?*/
+
+select actor_id as ID, first_name as "first name", last_name as "last name"
+from actor
+where first_name= "Joe";
