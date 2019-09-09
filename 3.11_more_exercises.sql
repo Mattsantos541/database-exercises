f14879a8d2e210dab0fab55e5bcad20a75890b8e
Matt Santos <mattsantos541@gmail.com>
2019-09-09 09:52:54 -0500

    #3

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -6,3 +6,8 @@ from countrylanguage
 join country
 on countrylanguage.`CountryCode` = country.`Code`
 where country.name = "Santa Monica";
+
+-- How many different countries are in each region?
+select country.region as Region, count(*) as num_countries
+from country
+group by  Region ORDER BY COUNT(*) asc;
