093a50a11b5c038bce4440e1f2a46810b6d31e29
Matt Santos <mattsantos541@gmail.com>
2019-09-09 09:34:36 -0500

    #2

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -1,2 +1,8 @@
 ###More Exercises
 
+-- What languages are spoken in Santa Monica?
+select countrylanguage.Language as Language, countrylanguage.Percentage
+from countrylanguage
+join country
+on countrylanguage.`CountryCode` = country.`Code`
+where country.name = "Santa Monica";
